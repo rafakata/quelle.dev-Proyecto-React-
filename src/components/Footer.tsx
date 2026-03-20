d5067e13c60fc3a68aad19f14ab885964127d3c2
rafakata <rafa.medina.quelle@gmail.com>
2026-03-20 19:21:38 +0100

    Actualización footer.

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import Logo from './Logo';
 const Footer: React.FC = () => {
   return (
     <footer className="footer">
-      <Logo scale={0.28} gradient />
+      <Logo scale={0.45} color="--q-black" />
       <div className="footer-links">
         <a href="https://www.linkedin.com/in/rafael-medina-quelle/" target="_blank" rel="noreferrer" className="footer-link">LinkedIn</a>
         <a href="https://github.com/rafakata" target="_blank" rel="noreferrer" className="footer-link">GitHub</a>
